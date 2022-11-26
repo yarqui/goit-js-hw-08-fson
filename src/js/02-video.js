8b96b874af4c1f7abc1adf043ccfc7cd9904b337
yarqui <y.pelykh@gmail.com>
2022-11-26 19:29:37 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,12 @@ const iframe = document.querySelector('iframe');
 
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
+});
+
+player.on('play', function () {
+  console.log('played the video!');
 });
 
 player.on('timeupdate', e => {
