d979ea99e062274fc96e1aac206a2be37375f280
yarqui <108986486+yarqui@users.noreply.github.com>
2022-11-27 11:34:55 +0000

    Deploying to gh-pages from @ yarqui/goit-js-hw-08@c5f454d942cc7b24e0eb8e0fa98bed31823607a7 🚀

---
+++ 03-feedback.ce99b5ab.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form");document.querySelector("input"),document.querySelector("textarea");e.addEventListener("input",(function(e){console.log(e.target.value),e.target.reset()}))}();
+//# sourceMappingURL=03-feedback.ce99b5ab.js.map
