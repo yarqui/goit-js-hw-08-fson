4f408d84308da103f00e534ad5276b742e279660
yarqui <108986486+yarqui@users.noreply.github.com>
2022-11-27 14:51:53 +0000

    Deploying to gh-pages from @ yarqui/goit-js-hw-08@6978eebf9b052b66846ffa2f46045121850e5b54 🚀

--- 03-feedback.7ccc1d44.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");document.querySelector("input"),document.querySelector("textarea");e.addEventListener("input",(e=>{let t="";t+=e.target.value,console.log(e.target.name,": ",e.target.value)}));
-//# sourceMappingURL=03-feedback.7ccc1d44.js.map
--- 03-feedback.d1734913.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");document.querySelector("input"),document.querySelector("textarea");e.addEventListener("input",(function(e){e.target.value,console.log(e.target.name,": ",e.target.value)}))}();
-//# sourceMappingURL=03-feedback.d1734913.js.map
