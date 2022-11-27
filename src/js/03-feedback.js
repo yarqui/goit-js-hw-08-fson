c5f454d942cc7b24e0eb8e0fa98bed31823607a7
yarqui <108986486+yarqui@users.noreply.github.com>
2022-11-27 13:34:32 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,9 @@
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('input');
+const textRef = document.querySelector('textarea');
 
+const onInput = e => {
+  console.log(e.target.value);
+  e.target.reset();
+};
+formRef.addEventListener('input', onInput);
